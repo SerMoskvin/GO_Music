e09d11036546254b6686389fb940e1991e62fa8f
SerMoskvin <168097590+SerMoskvin@users.noreply.github.com>
2025-07-29 19:16:13 +0300

    Delete GO_Music/engine/CRUD_inter.go

--- GO_Music/engine/CRUD_inter.go
+++
@@ -1,9 +0,0 @@
-package engine
-
-type Repository[T any] interface {
-	Create(entity *T) error
-	Update(entity *T) error
-	Delete(id int) error
-	GetByID(id int) (*T, error)
-	GetByIDs(ids []int) ([]*T, error)
-}
