55913912830e34188ec784aa72e15845ab5844e5
SerMoskvin <168097590+SerMoskvin@users.noreply.github.com>
2025-07-29 19:12:20 +0300

    Add custom time format
    Added transfer from ISO time for custom format (DD-MM-YYYY) and vice versa

--- GO_Music/domain/entity_inter.go
+++ GO_Music/domain/entity_inter.go
@@ -1,7 +1,19 @@
 package domain
 
+import "time"
+
 type Entity[ID comparable] interface {
 	GetID() ID
 	SetID(id ID)
 	Validate() error
+}
+
+// ParseDMY преобразует "DD.MM.YYYY" в time.Time
+func ParseDMY(dateStr string) (time.Time, error) {
+	return time.Parse("02.01.2006", dateStr)
+}
+
+// ToDMY преобразует time.Time в "DD.MM.YYYY"
+func ToDMY(t time.Time) string {
+	return t.Format("02.01.2006")
 }
